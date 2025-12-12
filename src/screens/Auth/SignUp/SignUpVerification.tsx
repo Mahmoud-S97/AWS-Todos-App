6264a5fe89d8fa084a814279693017143df19cb8
Mahmoud Saleh <mahmoudsaleh@MACBOOKPRO-5870.station>
2025-12-12 23:40:59 +0000

    Enhancements(NativeWind-Styles): Applied NativeWind dark/light mode on SignUpVerification-Screen

--- src/screens/Auth/SignUp/SignUpVerification.tsx
+++ src/screens/Auth/SignUp/SignUpVerification.tsx
@@ -41,18 +41,16 @@ const SignUpVerification = (props: Props): JSX.Element => {
 
   return (
 
-    <ScrollView className="flex-1 bg-orange-100" showsVerticalScrollIndicator={false}>
+    <ScrollView className="flex-1 bg-primary-light dark:bg-primary-dark" showsVerticalScrollIndicator={false}>
       <View className='flex-1 items-center justify-center'>
         <View className='w-[95%] h-full pb-[40] self-center mt-[100] items-center justify-center'>
-          <Text className='text-3xl text-gray-800 font-bold mb-[40]'>Code Verification</Text>
+          <Text className='text-3xl text-text-dark dark:text-text-light font-bold mb-[40]'>Code Verification</Text>
           <TextInput
-            className='h-[50] text-black-600 w-[95%] font-[500] border-b border-gray-500 p-2 mb-[30] text-start'
-            placeholderTextColor={'#444444'}
-            placeholder='Verify code from 6 digits'
+            className='h-[50] text-text-dark dark:text-text-light placeholder:text-text-dark placeholder:dark:text-text-light w-[95%] font-[500] border-b border-gray-500 dark:border-gray-700 p-2 mb-[30] text-start'            placeholder='Verify code from 6 digits'
             value={verificationCode}
             onChangeText={setVerificationCode}
           />
-          <MainButton style={[APP_THEME.mainShadow, { borderWidth: 1 }]} className='w-[96%] self-center my-[15] bg-orange-400 border border-gray-400 rounded-lg' textClassName='text-xl text-gray-900' onPress={verifySignUpCode}>
+          <MainButton style={[APP_THEME.mainShadow, { borderWidth: 1 }]} className='w-[96%] self-center my-[15] bg-primary-default border border-gray-400 dark:border-gray-700 rounded-lg' textClassName='text-xl text-gray-900' onPress={verifySignUpCode}>
             Confirm Code
           </MainButton>
         </View>
