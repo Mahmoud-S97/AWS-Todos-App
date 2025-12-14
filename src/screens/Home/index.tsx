92f11793e460d6fe2741d86b0956bbe1585d4362
Mahmoud Saleh <mahmoudsaleh@Mahmouds-MacBook-Pro.local>
2025-12-14 14:19:21 +0000

    Clean(HomeScreen): Formatted the code spaces and tabs for good readability

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -50,7 +50,8 @@ const HomeScreen = (props: Props): JSX.Element => {
                 }) as GraphQLResult<any>;
                 const { data } = results;
                 if (data?.listTodos?.items?.length) {
-                    const sortedTodoList = data?.listTodos?.items.sort((a: TodoList, b: TodoList) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime());                    setTodoList(sortedTodoList);
+                    const sortedTodoList = data?.listTodos?.items.sort((a: TodoList, b: TodoList) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime());
+                    setTodoList(sortedTodoList);
                 }
             } catch (error) {
                 console.log('Error while getting Todos: ', error);
